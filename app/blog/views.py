61a0cd399da793bb841557d1adcb564392c161aa
Agustin Mondani <a.mondani@hotmail.com>
2022-11-06 21:21:14 -0300

    primer commit de la entrega

---
+++ app/blog/views.py
@@ -0,0 +1,42 @@
+from django.core.paginator import Paginator
+from django.forms import model_to_dict
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
+
+from blog.forms import FormSuscriber
+from posts.models import Post
+
+
+def index(request):
+    if request.method == 'POST':
+        formSuscriber = FormSuscriber(request.POST)
+        if formSuscriber.is_valid():
+            instance = formSuscriber.save(commit=False)
+            instance.save()
+            return JsonResponse(model_to_dict(instance, fields=['nombre']), status=201)
+        else:
+            return JsonResponse(formSuscriber.errors, safe=False, status=200)
+
+    formSuscriber = FormSuscriber()
+
+    latest_posts = Post.objects.all()
+    paginator = Paginator(latest_posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'index.html', {
+        'formSuscriber': formSuscriber,
+        'page_obj': page_obj
+    })
+
+
+def about(request):
+    return render(request, 'acercade.html')
+
+
+def buscar(request):
+    if request.method == 'POST':
+        posts = Post.objects.filter(content__contains=request.POST.get('filtro'))
+        return render(request, 'busqueda.html', {'posts': posts})
+
+    return redirect('home')
